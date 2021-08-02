1919e34594b8a574355f1118d8708d76e7ce54a5
ToastyIQ <andrew.amposta@iqmetrix.com>
2021-08-02 17:30:53 -0500

    Dictionary tests working and updated csproj to use netcoreapp3.0

--- 2 - ListsLogicAndIterations/Code/Dictionarys.cs
+++ 2 - ListsLogicAndIterations/Code/Dictionarys.cs
@@ -4,20 +4,23 @@ using System.Collections.Generic;
 namespace ListsLogicAndIterations.Code
 {
 	public static class Dictionarys
-	{
+    {
 		public static Dictionary<Guid, decimal> Build(Guid key, decimal value)
-		{
-			throw new NotImplementedException();
-		}
+        {
+			Dictionary<Guid, decimal> dict = new Dictionary<Guid, decimal>();
+            return dict;
+        }
 
 		public static decimal GetValue(Dictionary<Guid, decimal> dict, Guid key)
-		{
-			throw new NotImplementedException();
-		}
+        {
+			if (dict.ContainsKey(key)) return 1;
+            else return 0;
+        }
 
 		public static void AddValue(Dictionary<Guid, decimal> dict, Guid key, decimal value)
 		{
-			throw new NotImplementedException();
+            if (dict.ContainsKey(key)) return;
+            else dict.Add(key, value);
 		}
 	}
 }
