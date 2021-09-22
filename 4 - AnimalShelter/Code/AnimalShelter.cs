2c352d9e44484787a759ccbb3ac263da14567665
Andrew Amposta <Andrew.Amposta@iqmetrix.com>
2021-09-22 17:52:36 -0500

    Starting again

---
+++ 4 - AnimalShelter/Code/AnimalShelter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalShelter.Code
+{
+    public class AnimalShelter
+    {
+        public AddAnimal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GetAnimal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FindAnimal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FindAnimalById()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
