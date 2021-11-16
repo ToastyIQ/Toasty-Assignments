2053fe246e56229c215a7a1d017546c52467787f
ToastyIQ <andrew.amposta@iqmetrix.com>
2021-11-16 11:05:53 -0600

    remove unnecessary classes

---
+++ 5 - RPG/Code/RPG/Code/Character/CharacterBuilder.cs
@@ -0,0 +1,20 @@
+﻿using RPG.Code.Character.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPG.Code.Character
+{
+    class CharacterBuilder : ICharacter
+    {
+        public int HeadRoundness { get; private set; }
+        public string FitnessLevel { get; private set; }
+        public int Height { get; private set; }
+        public int Intelligence { get; private set; }
+        public int Wisdom { get; private set; }
+        public int Dexterity { get; private set; }
+        public int Strength { get; private set; }
+        public int Charisma { get; private set; }
+        public int Constitution { get; private set; }
+    }
+}
